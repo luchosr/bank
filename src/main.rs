3a02c8e30d2e84904b6acb650ac6e909da277775
Luciano Ramello <luchosr@gmail.com>
2024-10-24 17:53:24 +0200

    feat: creates account and bank implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Bank {
+        Bank { accounts: vec![] }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    println!("{:#?}", bank);
 }
